3780329d1fde776fdd0bdb44e3541ab5ccbd06f4
timcrocodile <timcrocodile@proton.me>
2023-01-30 16:02:33 +0100

    30 gennaio 2023

--- lezione1/app.js
+++ lezione1/app.js
@@ -3,9 +3,12 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   // res.end("home");
+  //http://localhost:3000/
   if (req.url === "/") res.end("hello world");
-  else if (req.url === "/ospite") res.end("hello visitor");
-  else if (req.url === "/ciao") res.end("hello");
+  //http://localhost:3000/studenti
+  else if (req.url === "/studenti") res.end("hello visitor");
+  //http://localhost:3000/corsi
+  else if (req.url === "/corsi") res.end("hello");
   //404
   else {
     res.writeHead(404);
